c345b475ab44393c2df06aba43ff33f6878f8a96
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-03-17 21:48:27 +0100

    Update index.php
    Wrote a voting system to check if a voter is eligible to vote under ward 020

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-$name='Promise Sylvester Adiaha';
-echo $name;
+$voterAge= 15;
+$hasPVC=true;
+$voterWard= 020;
+if ($voterAge< 18) {
+    echo "Sorry, you must be at least 18 years old to vote.";
+  } else if (!$hasPVC) {
+    echo "Sorry, you must have a Permanent Voter's Card (PVC) to vote.";
+  } else if ($voterWard !== 020) {
+    echo "Sorry, you are not registered to vote in this ward.";
+  } else {
+    echo "Voter eligible to vote.";
+  }
 ?>
